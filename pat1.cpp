ca396c84e90b80f3e57a24dc25df85f412bd60fa
Abhigyankrsingh <singhabhigyankumar@gmail.com>
2023-11-26 20:35:15 +0530

    Pattern Coding

---
+++ pat1.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+
+
+void print1(int n){
+
+    for(int i =1; i<=n; i++){
+        for(int j =1; j<=n;j++){
+            cout << "x";
+        }
+
+        cout <<endl;
+    }
+}
+
+int main(){
+
+int n=4;
+
+print1(n);
+
+}
